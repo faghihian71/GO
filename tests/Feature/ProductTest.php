d361c5ffad5c0867a09c5410d714ba9a343cdead
babakfaghihian <faghihian71@gmail.com>
2020-01-24 16:35:22 +0330

    Feature test can get list of products

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductTest extends TestCase
+{
+    private $testCreationData;
+    private $faker;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function  setUp()
+    {
+        parent::setUp();
+
+        // make fake data
+        $this->faker = \Faker\Factory::create();
+        $this->testCreationData = [
+            'title' => $this->faker->name,
+            'price' => $this->faker->numberBetween(1, 100000)
+        ];
+
+
+    }
+
+
+    public function testCreateAProduct(){
+
+        $response = $this->post('/api/product',
+            $this->testCreationData);
+        $response->assertStatus(201);
+
+    }
+}
